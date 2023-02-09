065de0000bd17d9c6117d7071a9ecf396e8459e5
Nicolas Riscalas <nicolas.riscalas@stu.ocsb.ca>
2023-02-09 02:54:04 +0000

    fghjfgh

---
+++ HelloWorldStyle.swift
@@ -0,0 +1,12 @@
+//
+//  HelloWorldStyle.swift
+//
+//  Created by Nicolas Riscalas
+//  Created on 2023-02-08
+//  Version 1.0
+//  Copyright (c) 2023 Nicolas Riscalas. All rights reserved.
+//
+//  Prints what I want to do next year.
+
+print("Next year I plan on going to university")
+print("At the university I plan to study physics")
